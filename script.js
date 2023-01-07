fbe79412c65883b02d9c0494a34b237684d706b7
Jeramiha Holt <114194665+EzClops@users.noreply.github.com>
2023-01-06 17:16:14 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const ham = document.querySelector('.header .nav-bar .nav-list .ham');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
 ham.addEventListener('click', ()=>{
@@ -15,4 +16,11 @@ document.addEventListener('scroll', () => {
     }else{
         header.style.backgroundColor = "transparent";
     }
-})
+});
+
+menu_item.forEach(item => {
+    item.addEventListener('click', ()=> {
+        ham.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
