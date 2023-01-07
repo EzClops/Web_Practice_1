4433d77f228df36760c76bc9b9c2348842374ccb
Jeramiha Holt <114194665+EzClops@users.noreply.github.com>
2023-01-06 17:09:36 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const ham = document.querySelector('.header .nav-bar .nav-list .ham');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
+
+ham.addEventListener('click', ()=>{
+    ham.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+    
+})
+
+document.addEventListener('scroll', () => {
+    let scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = "#29323c";
+    }else{
+        header.style.backgroundColor = "transparent";
+    }
+})
